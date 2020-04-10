9cc60a6533fe0a6ad665eff4b8c78c038158eea8
Jiajie Hu <jiajie.hu@intel.com>
2020-04-10 21:59:10 +0800

    Add copyright notices and LICENSE

--- git-rebase/index.js
+++ git-rebase/index.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2020 The Aquarium Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 // This script should be skipped when running locally.
 
 // When running in GitHub hosted runners, the script writes the todo
