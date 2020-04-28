4571fd47fbcdaf5bcb4469e4812d28af644bf993
Jiajie Hu <jiajie.hu@intel.com>
2020-04-28 14:08:11 +0800

    Re-visit the copyleft of first party code

--- src/aquarium-direct-map/Uniform.cpp
+++ src/aquarium-direct-map/Uniform.cpp
@@ -3,6 +3,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
+
 #include "Uniform.h"
 
 Uniform::Uniform(const std::string &name, GLenum type, int length, int size, GLint index)
