webatintel/aquarium

commit cef949ea7871c4de1847212ab76f276fba316717

Roll build and Vulkan Validation Layers
This manual intervention is needed to build with updated Windows SDK:

  https://chromium-review.googlesource.com/c/chromium/src/+/2268383
  https://github.com/KhronosGroup/Vulkan-ValidationLayers/pull/1922

DEPS
@@ -12,14 +12,14 @@ vars = {
   'angle_root': 'third_party/angle',
   'angle_revision': '6c824a1bc17b286b86cf05a0228ec549875351eb',
   'glslang_revision': 'b5f003d7a3ece37db45578a8a3140b370036fc64',
-  'build_revision': '896323eeda1bd1b01156b70625d5e14de225ebc3',
+  'build_revision': '6c8a5ae8ab293db1096590f234de8c04efb91606',
   'buildtools_revision': 'c8f5482fd0c9ec93bc4312a119982888c4df1c3d',
   'tools_clang_revision': '698732d5db36040c07d5cc5f9137fcc943494c11',
   'spirv_tools_revision': '85f3e93d13f32d45bd7f9999aa51baddf2452aae',
   'jsoncpp_revision': '571788934b5ee8643d53e5d054534abbe6006168',
   'jsoncpp_source_revision': '645250b6690785be60ab6780ce4b58698d884d11',
   'vulkan-headers_revision': '09531f27933bf04bffde9074acb302e026e8f181',
-  'vulkan-validation-layers_revision': '31bdb2d4376919f5964dd8314dd2192810f08b36',
+  'vulkan-validation-layers_revision': '5fab7f82a5486f8d36d6761dffcb3a149c9b0895',
   'cxxopts_revision': '07f5cb24f1d75aad6c27eafd83863a78a37f16cb',
 }
 

commit 3c75ed43e96c792eebf2c40371bac551f87a6636

Generate dummy gclient_args.gni
Fixes a macOS build regression caused by #181

DEPS
@@ -4,6 +4,10 @@
 # found in the LICENSE file.
 #
 
+gclient_gn_args_file = 'build/config/gclient_args.gni'
+gclient_gn_args = [
+]
+
 vars = {
   'chromium_git': 'https://chromium.googlesource.com',
   'github_git': 'https://github.com',